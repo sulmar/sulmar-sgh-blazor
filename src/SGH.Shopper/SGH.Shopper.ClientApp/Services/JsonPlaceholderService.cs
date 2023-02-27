242cc363cc9e8dee762c015c507145070e81ba3c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-27 14:52:17 +0100

    Przekazywanie parametru

--- src/SGH.Shopper/SGH.Shopper.ClientApp/Services/JsonPlaceholderService.cs
+++ src/SGH.Shopper/SGH.Shopper.ClientApp/Services/JsonPlaceholderService.cs
@@ -6,6 +6,7 @@ namespace SGH.Shopper.ClientApp.Services;
 public interface IUserService
 {
     Task<IEnumerable<User>> GetUsers();
+    Task<User> GetUserById(int id);
 }
 
 public class JsonPlaceholderService : IUserService
@@ -13,4 +14,5 @@ public class JsonPlaceholderService : IUserService
     private readonly HttpClient client;
     public JsonPlaceholderService(HttpClient client) => this.client = client;
     public async Task<IEnumerable<User>> GetUsers() => await client.GetFromJsonAsync<IEnumerable<User>>("users");
+    public async Task<User> GetUserById(int id) => await client.GetFromJsonAsync<User>($"users/{id}");
 }
