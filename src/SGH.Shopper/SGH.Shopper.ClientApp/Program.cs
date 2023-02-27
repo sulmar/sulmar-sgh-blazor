e509465b40e6212e5c44447fbec6c887504ec0fa
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-27 13:21:17 +0100

    Refaktoring

--- src/SGH.Shopper/SGH.Shopper.ClientApp/Program.cs
+++ src/SGH.Shopper/SGH.Shopper.ClientApp/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SGH.Shopper.ClientApp;
+using SGH.Shopper.ClientApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com/") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// dotnet add package Microsoft.Extensions.Http
+builder.Services.AddHttpClient<JsonPlaceholderService>(sp => sp.BaseAddress = new Uri("https://jsonplaceholder.typicode.com"));
 
 await builder.Build().RunAsync();
---
+++ src/SGH.Shopper/SGH.Shopper.ClientApp/Services/JsonPlaceholderService.cs
@@ -0,0 +1,16 @@
+﻿using SGH.Shopper.Domain;
+using System.Net.Http.Json;
+
+namespace SGH.Shopper.ClientApp.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetUsers();
+}
+
+public class JsonPlaceholderService : IUserService
+{
+    private readonly HttpClient client;
+    public JsonPlaceholderService(HttpClient client) => this.client = client;
+    public async Task<IEnumerable<User>> GetUsers() => await client.GetFromJsonAsync<IEnumerable<User>>("users");
+}
